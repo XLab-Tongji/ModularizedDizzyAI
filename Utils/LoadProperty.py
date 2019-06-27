0ecb6b59f117db45639f5c87e71d072c2e2e5a2d
niyw <nee_11235@qq.com>
2019-06-27 15:51:34 +0800

    format

--- Utils/LoadProperty.py
+++ Utils/LoadProperty.py
@@ -9,17 +9,19 @@ def load_property(name):
 
 # 测试主函数
 if __name__ == '__main__':
-    # from stanfordcorenlp import StanfordCoreNLP
-    # with StanfordCoreNLP(r'E:/stanford-corenlp-full-2018-10-05', lang='zh', memory='4g', quiet=True) as nlp:
-    #     nlp.parse("test")
+    from stanfordcorenlp import StanfordCoreNLP
+    with StanfordCoreNLP(r'E:/stanford-corenlp-full-2018-10-05', lang='zh', memory='4g', quiet=True) as nlp:
+        nlp.parse("test")
 
-    config = load_property("leave")
-    module = importlib.import_module(config["file"])
-    while True:
-        print("你要做什么呢")
-        sentence = input()
-        if module.entry(sentence):
-            message = module.ask(sentence, config["component"])
-            print(module.format(message))
-            print("-----------------")
+        from Staff import Staff
+        staff = Staff("001", "倪奕玮", "徐锦程", "同济大学")
+        config = load_property("leave")
+        module = importlib.import_module(config["file"])
+        while True:
+            print("你要做什么呢")
+            sentence = input()
+            if module.entry(sentence):
+                message = module.ask(sentence, config["component"])
+                print(staff.toString() + module.format(message))
+                print("-----------------")
 
--- Utils/Staff.py
+++ Utils/Staff.py
@@ -1,9 +1,9 @@
 class Staff:
-    def __init__(self, name, examine_person, department):
-        self.id = id
+    def __init__(self, staff_id, name, examine_person, department):
+        self.staff_id = staff_id
         self.name = name
         self.examine_person = examine_person
         self.department = department
 
     def toString(self):
-        return "#" + str(self.id) + "#" + self.examine_person + "@" + self.name + "# "
+        return "#" + str(self.staff_id) + "#" + self.examine_person + "@" + self.name + "# "
